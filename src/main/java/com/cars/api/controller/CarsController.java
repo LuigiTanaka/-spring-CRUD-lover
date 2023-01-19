0d8b1235a66c63f154b414ae977d83b1600c2c8e
LuigiTanaka <luigi.tanaka12@gmail.com>
2023-01-19 17:43:52 -0300

    resolução CRUD lover

---
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -0,0 +1,55 @@
+package com.cars.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cars.api.dto.CarsDTO;
+import com.cars.api.model.Car;
+import com.cars.api.repository.CarsRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarsController {
+
+    @Autowired
+    private CarsRepository repository;
+
+    @GetMapping
+    public List<Car> listAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public void create(@RequestBody @Valid CarsDTO req) {
+        repository.save(new Car(req));
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody @Valid CarsDTO req) {
+        repository.findById(id).map(car ->  {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/cars/api/dto/CarsDTO.java
@@ -0,0 +1,25 @@
+package com.cars.api.dto;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+public record CarsDTO(
+    @NotBlank
+    String modelo, 
+
+    @NotBlank
+    String fabricante, 
+
+    @Past
+    Date dataFabricacao, 
+
+    @NotNull
+    double valor, 
+
+    @NotNull
+    Integer anoModelo
+    ) {
+}
